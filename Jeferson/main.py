0e1b1d59f0d17d6f42cf2bcac70a7d925ac49cbf
Jeferson <jefmaarques@outlook.com>
2019-10-23 13:30:40 -0300

    Correction on main function call

--- Jeferson/main.py
+++ Jeferson/main.py
@@ -2,5 +2,5 @@ from deapForL2r import main
 
 for fold in range(1, 6):
     for params in [['precision'], ['risk'], ['precision, risk']]:
-        main(DATASET='web10k', FOLD=str(fold), METHOD='spea2', PARAMS=params,
+        main(DATASET='web10k', NUM_FOLD=str(fold), METHOD='spea2', PARAMS=params,
              NUM_GENES=10000, NUM_INDIVIDUOS=100, NUM_GENERATIONS=50)
