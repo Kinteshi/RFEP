67f57cc4ca2603e01b2c3d0a8e6448654c8f13fe
Jeferson <36821534+Kinteshi@users.noreply.github.com>
2020-02-12 19:57:51 -0300

    Memory eval

--- GARF/timeEvaluation.py
+++ GARF/timeEvaluation.py
@@ -4,6 +4,9 @@ import numpy as np
 from l2rCodesSerial import load_L2R_file
 import pickle
 import matplotlib.pyplot as plt
+import psutil
+import os
+
 dataset = 'web10k'
 
 
@@ -20,6 +23,11 @@ def wrapper(func, *args, **kwargs):
 no_buffer = []
 buffer = []
 trees = []
+no_buffer_mem = []
+buffer_mem = []
+pid = os.getpid()
+py = psutil.Process(pid)
+
 
 for trees in np.arange(100, 1000, 100):
     model = Forest(n_estimators=trees, max_features=0.3, max_leaf_nodes=100, min_samples_leaf=1,
@@ -29,16 +37,20 @@ for trees in np.arange(100, 1000, 100):
     evaluating1 = wrapper(model.oob_predict, X_train, y_train,
                           ('1' * trees) + ('0' * (5000-trees)), False)
     time1 = timeit.timeit(evaluating1, number=1)
+    memoryUse = py.memory_info()[0] / 2.**30
+    no_buffer_mem.append(memoryUse)
 
     model.oob_predict_buffer(X_train, y_train)
-
     evaluating2 = wrapper(model.oob_buffered_predict, list(
         ('1' * trees) + ('0' * (5000-trees))))
     time2 = timeit.timeit(evaluating2, number=1)
+    memoryUse = py.memory_info()[0] / 2.**30
+    buffer_mem.append(memoryUse)
 
     no_buffer.append(time1)
     buffer.append(time2)
     trees.append(trees)
+
 
 fig = plt.figure(figsize=(15, 15))
 
@@ -53,11 +65,30 @@ ax.set_xlabel('Trees')
 ax.set_ylabel('Time')
 
 ax.legend(['No buffer', 'Buffer'], loc='upper left')
+ax.set_title('Time')
 
 plt.savefig('TimeTest.png')
+
+ax.cla()
+
+ax.plot(no_buffer_mem, trees, color='green')
+ax.plot(buffer_mem, trees, color='blue')
+
+ax.set_xlim(np.max(trees), np.min(trees))
+ax.set_ylim(np.max(no_buffer_mem + buffer_mem),
+            np.min(no_buffer_mem + buffer_mem))
+ax.set_xlabel('Trees')
+ax.set_ylabel('Memory')
+
+ax.legend(['No buffer', 'Buffer'], loc='upper left')
+ax.set_title('Memory')
+
+plt.savefig('MemTest.png')
 
 json_holder = {}
 
 json_holder['nobuffer'] = no_buffer
 json_holder['buffer'] = buffer
 json_holder['trees'] = trees
+json_holder['buffer_mem'] = buffer_mem
+json_holder['no_buffer_mem'] = no_buffer_mem
